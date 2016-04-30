bba07a6cb0040578b559ab3cebcc7a2186dbad13
IanPaice <ianpaicefoz@gmail.com>
2016-05-01 01:42:39 +0200

    all changes

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,13 @@ var interstitialAdUnit = "ca-app-pub-4906074177432504/1649035673";
 var isOverlap = true; //true: overlap, false: split
 var isTest = false;
 
+var url_c1 = "http://radioserver01.ccradio.es/adminradio/info/info_stream.php?stream=canal_1";
+var url_c2 = "http://radioserver01.ccradio.es/adminradio/info/info_stream.php?stream=canal_2";
+var url_c3 = "http://radioserver01.ccradio.es/adminradio/info/info_stream.php?stream=canal_3";
+
+var song_c1;
+var song_c2;
+var song_c3;
 
 document.addEventListener("deviceready", function(){
 	window.admob.setUp(bannerAdUnit, interstitialAdUnit, isOverlap, isTest);
@@ -43,9 +50,9 @@ $(document).ready( function() {
     $.mobile.allowCrossDomainPages = true;
     $.mobile.pushStateEnabled      = false;
 	
-	$.ajax({
+	/*$.ajax({
 		type: "GET",
-		url: "http://radioserver01.ccradio.es/adminradio/info/info_stream.php?stream=canal_1",
+		url: url_c1,
 		dataType: "json",
         success: function(data) { 
 			//alert(JSON.stringify(data));		
@@ -54,6 +61,7 @@ $(document).ready( function() {
 			var json = $.parseJSON(JSON.stringify(data));
 			//now json variable contains data in json format
 			//let's display a few items
+			song_c1 = json.song;
 			
 			$('#playing_c1').html('<center><img src="'+ json.img_album +'" width="150">' + "<br>"
 								+ 'Cancion: ' + json.song + "<br>"
@@ -64,7 +72,7 @@ $(document).ready( function() {
 
 	$.ajax({
 		type: "GET",
-		url: "http://radioserver01.ccradio.es/adminradio/info/info_stream.php?stream=canal_2",
+		url: url_c2,
 		dataType: "json",
         success: function(data) { 
 			//alert(JSON.stringify(data));		
@@ -73,6 +81,7 @@ $(document).ready( function() {
 			var json = $.parseJSON(JSON.stringify(data));
 			//now json variable contains data in json format
 			//let's display a few items
+			song_c2 = json.song;
 			
 			$('#playing_c2').html('<center><img src="'+ json.img_album +'" width="150">' + "<br>"
 								+ 'Cancion: ' + json.song + "<br>"
@@ -83,7 +92,7 @@ $(document).ready( function() {
 
 	$.ajax({
 		type: "GET",
-		url: "http://radioserver01.ccradio.es/adminradio/info/info_stream.php?stream=canal_3",
+		url: url_c3,
 		dataType: "json",
         success: function(data) { 
 			//alert(JSON.stringify(data));		
@@ -92,13 +101,14 @@ $(document).ready( function() {
 			var json = $.parseJSON(JSON.stringify(data));
 			//now json variable contains data in json format
 			//let's display a few items
+			song_c3 = json.song;
 			
 			$('#playing_c3').html('<center><img src="'+ json.img_album +'" width="150">' + "<br>"
 								+ 'Cancion: ' + json.song + "<br>"
 								+ 'Album: ' + json.album + "<br>"
 								+ 'Autor: ' + json.autor + "</center>");
         }
-	});	
+	});	*/
 
 	$(".nav_button").click(function(){
 		if($(".menu").css('display') == 'none'){
@@ -111,6 +121,51 @@ $(document).ready( function() {
 	$(".link_button").click(function(){
 		$(".menu").hide();
 	});
+	
+	setInterval(function(){ reloadInfoMusic(); }, 5000);
+	
+	function reloadInfoMusic(){
+		//alert("Prueba" + url_song_c1 + url_song_c2 + url_song_c3);
+		$.getJSON(url_c1, function(c1_data){
+			if(song_c1 != c1_data.song)
+			{
+				song_c1 = c1_data.song;
+				$("#info_music_c1").append("I");
+				$('#playing_c1').html('<center><img src="'+ c1_data.img_album +'" width="150">' + "<br>"
+									+ 'Cancion: ' + c1_data.song + "<br>"
+									+ 'Album: ' + c1_data.album + "<br>"
+									+ 'Autor: ' + c1_data.autor + "</center>");
+				$('#c1_played').append('<div><center><img src="'+ c1_data.img_album + '" width="50"><br>'+ c1_data.album +'</center></div>');								
+			}
+		});
+		
+		$.getJSON(url_c2, function(c2_data){
+			if(song_c2 != c2_data.song)
+			{
+				song_c2 = c2_data.song;
+				$("#info_music_c2").append("I");
+				$('#playing_c2').html('<center><img src="'+ c2_data.img_album +'" width="150">' + "<br>"
+									+ 'Cancion: ' + c2_data.song + "<br>"
+									+ 'Album: ' + c2_data.album + "<br>"
+									+ 'Autor: ' + c2_data.autor + "</center>");
+				$('#c2_played').append('<div><center><img src="'+ c2_data.img_album + '" width="50"><br>'+ c2_data.album +'</center></div>');									
+			}
+		});
+		
+		$.getJSON(url_c3, function(c3_data){
+			if(song_c3 != c3_data.song)
+			{
+				song_c3 = c3_data.song;
+				$("#info_music_c3").append("I");
+				$('#playing_c3').html('<center><img src="'+ c3_data.img_album +'" width="150">' + "<br>"
+									+ 'Cancion: ' + c3_data.song + "<br>"
+									+ 'Album: ' + c3_data.album + "<br>"
+									+ 'Autor: ' + c3_data.autor + "</center>");
+				$('#c3_played').append('<div><center><img src="'+ c3_data.img_album + '" width="50"><br>'+ c3_data.album +'</center></div>');										
+			}
+		});
+		
+	};
 	
 /*	var mySwiper = new Swiper ('.swiper-container', {
       // Optional parameters
