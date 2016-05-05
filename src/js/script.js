dd2449db0d18b38338a07a80c82587ab91efb883
IanPaice <ianpaicefoz@gmail.com>
2016-05-05 17:06:00 +0200

    finalizando

--- src/js/script.js
+++ src/js/script.js
@@ -50,66 +50,6 @@ $(document).ready( function() {
     $.mobile.allowCrossDomainPages = true;
     $.mobile.pushStateEnabled      = false;
 	
-	/*$.ajax({
-		type: "GET",
-		url: url_c1,
-		dataType: "json",
-        success: function(data) { 
-			//alert(JSON.stringify(data));		
-			//data downloaded so we call parseJSON function 
-			//and pass downloaded data
-			var json = $.parseJSON(JSON.stringify(data));
-			//now json variable contains data in json format
-			//let's display a few items
-			song_c1 = json.song;
-			
-			$('#playing_c1').html('<center><img src="'+ json.img_album +'" width="150">' + "<br>"
-								+ 'Cancion: ' + json.song + "<br>"
-								+ 'Album: ' + json.album + "<br>"
-								+ 'Autor: ' + json.autor + "</center>");
-        }
-	});
-
-	$.ajax({
-		type: "GET",
-		url: url_c2,
-		dataType: "json",
-        success: function(data) { 
-			//alert(JSON.stringify(data));		
-			//data downloaded so we call parseJSON function 
-			//and pass downloaded data
-			var json = $.parseJSON(JSON.stringify(data));
-			//now json variable contains data in json format
-			//let's display a few items
-			song_c2 = json.song;
-			
-			$('#playing_c2').html('<center><img src="'+ json.img_album +'" width="150">' + "<br>"
-								+ 'Cancion: ' + json.song + "<br>"
-								+ 'Album: ' + json.album + "<br>"
-								+ 'Autor: ' + json.autor + "</center>");
-        }
-	});
-
-	$.ajax({
-		type: "GET",
-		url: url_c3,
-		dataType: "json",
-        success: function(data) { 
-			//alert(JSON.stringify(data));		
-			//data downloaded so we call parseJSON function 
-			//and pass downloaded data
-			var json = $.parseJSON(JSON.stringify(data));
-			//now json variable contains data in json format
-			//let's display a few items
-			song_c3 = json.song;
-			
-			$('#playing_c3').html('<center><img src="'+ json.img_album +'" width="150">' + "<br>"
-								+ 'Cancion: ' + json.song + "<br>"
-								+ 'Album: ' + json.album + "<br>"
-								+ 'Autor: ' + json.autor + "</center>");
-        }
-	});	*/
-
 	$(".nav_button").click(function(){
 		if($(".menu").css('display') == 'none'){
 			$(".menu").show();
@@ -124,18 +64,33 @@ $(document).ready( function() {
 	
 	setInterval(function(){ reloadInfoMusic(); }, 5000);
 	
+	var c1_albums = 0;
+	var c1_aux = 1;
+		
+	var c2_albums = 0;
+	var c2_aux = 1;
+		
+	var c3_albums = 0;
+	var c3_aux = 1;
+	
 	function reloadInfoMusic(){
 		//alert("Prueba" + url_song_c1 + url_song_c2 + url_song_c3);
 		$.getJSON(url_c1, function(c1_data){
 			if(song_c1 != c1_data.song)
 			{
 				song_c1 = c1_data.song;
-				$("#info_music_c1").append("I");
+				c1_albums = c1_albums + 1;
+				if(c1_albums > 3)
+				{
+					$("#c1_" + c1_aux).remove();
+					c1_aux = c1_aux + 1;
+				}
+				//$("#info_music_c1").append("I" + c1_albums);
 				$('#playing_c1').html('<center><img src="'+ c1_data.img_album +'" width="150">' + "<br>"
 									+ 'Cancion: ' + c1_data.song + "<br>"
 									+ 'Album: ' + c1_data.album + "<br>"
 									+ 'Autor: ' + c1_data.autor + "</center>");
-				$('#c1_played').append('<div style="float:left;text-align:center;margin-left:5px;"><img src="'+ c1_data.img_album + '" width="50"><br>'+ c1_data.song +'<br>'+ c1_data.album +'</div>');								
+				$('#c1_played').append('<div id="c1_'+c1_albums+'" style="float:left;text-align:center;margin-left:5px;width:32%;"><img src="'+ c1_data.img_album + '" width="50"><br>'+ c1_data.song +'<br>'+ c1_data.album +'</div>');								
 			}
 		});
 		
@@ -143,12 +98,18 @@ $(document).ready( function() {
 			if(song_c2 != c2_data.song)
 			{
 				song_c2 = c2_data.song;
-				$("#info_music_c2").append("I");
+				c2_albums = c2_albums + 1;
+				if(c2_albums > 3)
+				{
+					$("#c2_" + c2_aux).remove();
+					c2_aux = c2_aux + 1;
+				}				
+				//$("#info_music_c2").append("I");
 				$('#playing_c2').html('<center><img src="'+ c2_data.img_album +'" width="150">' + "<br>"
 									+ 'Cancion: ' + c2_data.song + "<br>"
 									+ 'Album: ' + c2_data.album + "<br>"
 									+ 'Autor: ' + c2_data.autor + "</center>");
-				$('#c2_played').append('<div style="float:left;text-align:center;margin-left:5px;"><img src="'+ c2_data.img_album + '" width="50"><br>'+ c2_data.song +'<br>'+ c2_data.album +'</div>');									
+				$('#c2_played').append('<div id="c1_'+c1_albums+'" style="float:left;text-align:center;margin-left:5px;width:32%;"><img src="'+ c2_data.img_album + '" width="50"><br>'+ c2_data.song +'<br>'+ c2_data.album +'</div>');									
 			}
 		});
 		
@@ -156,12 +117,18 @@ $(document).ready( function() {
 			if(song_c3 != c3_data.song)
 			{
 				song_c3 = c3_data.song;
-				$("#info_music_c3").append("I");
+				c3_albums = c3_albums + 1;
+				if(c3_albums > 3)
+				{
+					$("#c3_" + c3_aux).remove();
+					c3_aux = c3_aux + 1;
+				}	
+				//$("#info_music_c3").append("I");
 				$('#playing_c3').html('<center><img src="'+ c3_data.img_album +'" width="150">' + "<br>"
 									+ 'Cancion: ' + c3_data.song + "<br>"
 									+ 'Album: ' + c3_data.album + "<br>"
 									+ 'Autor: ' + c3_data.autor + "</center>");
-				$('#c3_played').append('<div style="float:left;text-align:center;margin-left:5px;"><img src="'+ c3_data.img_album + '" width="50"><br>'+ c3_data.song +'<br>'+ c3_data.album +'</div>');										
+				$('#c3_played').append('<div id="c3_'+c3_albums+'"style="float:left;text-align:center;margin-left:5px;width:32%;"><img src="'+ c3_data.img_album + '" width="50"><br>'+ c3_data.song +'<br>'+ c3_data.album +'</div>');										
 			}
 		});
 		
